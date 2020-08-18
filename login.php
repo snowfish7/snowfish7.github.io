081846af941ec6c252cf590f9eceba1441bfc2b2
snowfish7 <skyrider2003@gmail.com>
2020-08-18 11:30:31 -0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,13 @@
+<?php
+//start the session
+session_start();
+
+?>
+<html>
+	<head>
+		<link rel="stylesheet" href="styles.css">
+	</head>
+	<body>
+		<h1>Welcome to Snowfish Plus</h1>
+	</body>
+</html>
